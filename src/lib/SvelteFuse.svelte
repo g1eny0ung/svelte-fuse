b8ede5ef9965dc8c3d42c40b238b935be9915add
Yue Yang <g1enyy0ung@gmail.com>
2023-08-04 18:31:49 +0800

    chore: init

---
+++ src/lib/SvelteFuse.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" generics="T">
+  import Fuse from 'fuse.js'
+
+  export let list: ReadonlyArray<T>
+  export let options: Fuse.IFuseOptions<T>
+  export let query: string
+
+  export let result: Fuse.FuseResult<T>[]
+
+  $: fuse = new Fuse(list, options)
+  $: if (list) {
+    fuse.setCollection(list)
+  }
+  $: if (list || query) {
+    result = fuse.search(query)
+  }
+</script>
